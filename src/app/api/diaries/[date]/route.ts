b1b1b7eef1456c8a2d43691b81065447d1858dab
YuyaTerada1117 <[ahiboh.info@gmail.com]>
2025-04-20 19:19:07 +0900

    fix lint error 2

--- src/app/api/diaries/[date]/route.ts
+++ src/app/api/diaries/[date]/route.ts
@@ -1,10 +1,18 @@
-import { supabase } from '@/lib/supabase';
-export async function GET(_: Request, { params }: { params: { date: string } }) {
+import { NextRequest } from 'next/server';
+import { supabase }  from '@/lib/supabase';
+
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: { date: string } }
+) {
   const { data, error } = await supabase
     .from('diaries')
     .select('*')
     .eq('date', params.date)
     .single();
-  if (error) return new Response(error.message, { status: 404 });
+
+  if (error) {
+    return new Response(error.message, { status: 404 });
+  }
   return Response.json(data);
 }
